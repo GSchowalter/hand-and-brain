0f0ea64475b82c6bef40cc07eaf1647fd32b30c1
GSchowalter <grantschowalter@gmail.com>
2023-04-17 13:25:16 -0700

    I think I remember nextjs now

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { HttpClient, OAuth2AuthCodePKCE } from "@bity/oauth2-auth-code-pkce";
 import { useRouter } from "next/router";
+import { getAbsoluteUrl } from "@/utils/vercel-utilities";
 
 export default function Home() {
   const lichessHost = "https://lichess.org";
@@ -9,11 +10,41 @@ export default function Home() {
   let router = useRouter();
   let BASE_PATH = "";
   let clientUrl = "";
+
+  const authenticate = async (oauth) => {
+    const httpClient = oauth.decorateFetchHTTPClient(window.fetch);
+    const res = await httpClient(`${lichessHost}/api/account`);
+    const me = {
+      ...(await res.json()),
+      httpClient,
+    };
+    if (me.error) throw me.error;
+    // this.me = me;
+  };
+
+  async function init(oauth) {
+    try {
+      const accessContext = await oauth.getAccessToken();
+      if (accessContext) await authenticate();
+    } catch (err) {
+      console.error(err);
+    }
+    // TODO implement this in a react functional way
+    // if (!this.me) {
+    //   try {
+    //     const hasAuthCode = await oauth.isReturningFromAuthServer();
+    //     if (hasAuthCode) await authenticate();
+    //   } catch (err) {
+    //     console.error(err);
+    //   }
+    // }
+  }
+
   useEffect(() => {
     // const BASE_PATH = location.pathname.replace(/\/$/, "");
     // const clientUrl = `${location.protocol}//${location.host}${BASE_PATH || "/"}`;
     BASE_PATH = router.basePath;
-    clientUrl = router.asPath;
+    clientUrl = getAbsoluteUrl() + BASE_PATH;
 
     let oauth = new OAuth2AuthCodePKCE({
       authorizationUrl: `${lichessHost}/oauth`,
@@ -24,6 +55,8 @@ export default function Home() {
       onAccessTokenExpiry: (refreshAccessToken) => refreshAccessToken(),
       onInvalidGrant: console.warn,
     });
+
+    init(oauth);
   }, []);
 
   function handleClick() {
